e50c48088bbde0df57449abf3c6d561820f7077f
Evin Dunn <evindunn14@gmail.com>
2023-07-08 11:42:29 -0700

    Simplifies model

--- src/lifeModel.js
+++ src/lifeModel.js
@@ -1,5 +1,3 @@
-import {LifeCell} from "./cell";
-
 export class LifeModel {
     constructor(cellMatrix) {
         this.matrix = [];
@@ -43,6 +41,41 @@ export class LifeModel {
         this.matrix[rowIdx][colIdx] = cell === 0 ? 1 : 0;
     }
 
+    updateValue(rowIdx, colIdx) {
+        const currentVal = this.matrix[rowIdx][colIdx];
+        const prevRow = rowIdx - 1;
+        const nextRow = rowIdx + 1;
+        const prevCol = colIdx - 1;
+        const nextCol = colIdx + 1;
+
+        const topNeighborVal = this.valueAt(prevRow, colIdx);
+        const bottomNeighborVal = this.valueAt(nextRow, colIdx);
+        const leftNeighborVal = this.valueAt(rowIdx, prevCol);
+        const rightNeighborVal = this.valueAt(rowIdx, nextCol);
+
+        const topLeftNeighborVal = this.valueAt(prevRow, prevCol);
+        const topRightNeighborVal = this.valueAt(prevRow, nextCol);
+        const bottomLeftNeighborVal = this.valueAt(nextRow, prevCol);
+        const bottomRightNeighborVal = this.valueAt(nextRow, nextCol);
+
+        const neighborCount = (
+            topNeighborVal +
+            bottomNeighborVal +
+            leftNeighborVal +
+            rightNeighborVal +
+            topLeftNeighborVal +
+            topRightNeighborVal +
+            bottomLeftNeighborVal +
+            bottomRightNeighborVal
+        );
+        const underpopulated = currentVal === 1 && neighborCount < 2;
+        const staysLiving = currentVal === 1 && (neighborCount === 2 || neighborCount === 3)
+        const overpopulated = currentVal === 1 && neighborCount > 3;
+        const born = currentVal === 0 && neighborCount === 3;
+
+        return !underpopulated && !overpopulated && (staysLiving || born) ? 1 : 0;
+    }
+
     update() {
         const newVals = [];
         const newCol = new Array(this.numCols).fill(0);
@@ -50,12 +83,8 @@ export class LifeModel {
             newVals.push([...newCol]);
         }
         for (let rowIdx = 0; rowIdx < this.numRows; rowIdx++) {
-            const currentRow = this.matrix[rowIdx];
             for (let colIdx = 0; colIdx < this.numCols; colIdx++) {
-                const currentVal = currentRow[colIdx];
-                const cell = new LifeCell(rowIdx, colIdx, currentVal);
-                cell.updateAlive(this);
-                newVals[rowIdx][colIdx] = cell.value;
+                newVals[rowIdx][colIdx] = this.updateValue(rowIdx, colIdx);
             }
         }
         this.matrix = newVals;
