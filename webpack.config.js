c80d11c51a355801394e825278d1a0952fc4899c
Evin Dunn <evindunn14@gmail.com>
2025-08-28 20:39:57 -0400

    Webpack clean output

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     output: {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
